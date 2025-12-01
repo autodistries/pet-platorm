9115e6731081f324ae0940e84970233b62c8f50e
autodistries <66792425+autodistries@users.noreply.github.com>
2025-12-01 17:46:23 +0100

    remove deprecated sql scripts

--- scripts/generate-admin-hash.js
+++
@@ -1,9 +0,0 @@
-import('bcrypt').then(async ({ default: bcrypt }) => {
-  const password = 'Admin123!';
-  const saltRounds = 10;
-  const hash = await bcrypt.hash(password, saltRounds);
-  console.log('Password:', password);
-  console.log('Hash:', hash);
-  console.log('\nSQL:');
-  console.log(`UPDATE customers SET password_hash = '${hash}' WHERE email = 'admin@petshop.com';`);
-});
